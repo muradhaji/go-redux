109c4c552f94d7b1d13f7d081bb2c7e83da38bc9
muradhaji <murad.haciyev00@gmail.com>
2022-05-31 18:38:51 +0300

    redux basics practised

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,8 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
 import counterSlice from './Slices/counterSlice';
 
 export default configureStore({
   reducer: {
     counter: counterSlice,
+  },
+  devTools: process.env.NODE_ENV !== 'production',
+  middleware: (getDefaultMiddleware) => [...getDefaultMiddleware(), logger],
+  preloadedState: {
+    counter: {
+      value: 13,
+    },
   },
 });
